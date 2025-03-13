102c66754dd65630c70c94c73f9148ce6170c2bd
cw3l <cw3l@MacBook-Pro-de-cw3l.local>
2025-03-13 23:13:52 +0100

    /2025-03-13T23:13:52+01:00

---
+++ include/main.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cw3l <cw3l@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/13 22:15:28 by cw3l              #+#    #+#             */
+/*   Updated: 2025/03/13 22:24:01 by cw3l             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <dirent.h>
+
+
+
+
+#endif 
