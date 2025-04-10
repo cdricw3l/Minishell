6d3a9aec5f17788afbd7259a6d4f465689164359
cb <cdricw3l@gmail.com>
2025-04-10 10:21:45 +0200

    minishell/cd_ok/2025-04-10T10:21:45+02:00

--- srcs/builtin/cd.c
+++ srcs/builtin/cd.c
@@ -6,7 +6,7 @@
 /*   By: cw3l <cw3l@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/26 12:30:49 by cbouhadr          #+#    #+#             */
-/*   Updated: 2025/04/10 08:04:01 by cw3l             ###   ########.fr       */
+/*   Updated: 2025/04/10 10:21:25 by cw3l             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,20 +43,52 @@ char	*ft_get_home_path(char *path, int cd)
 		total_path = env;
 	if (!total_path)
 		return (NULL);
-	path = total_path;
-	return (path);
+	return (total_path);
+}
+
+int ft_check_path(char *str)
+{
+	char **split;
+
+	split = ft_split(str, 32);
+	if(ft_get_split_len(split) <=2 && ft_get_split_len(split) > 0)
+		return(1);
+	if(ft_get_split_len(split) == 3)
+		printf("cd: string not in pwd: %s\n", split[1]);
+	if(ft_get_split_len(split) > 3)
+		printf("cd: too many arguments");
+	ft_split_clean(&split);
+	return(0);
+}
+
+int ft_return_to_home(char *path)
+{
+	char	*home_path;
+	int		chdir_return;
+	
+	chdir_return = 1;
+	home_path = ft_get_home_path(path, 1);
+	if(home_path)
+		chdir_return = chdir(home_path);
+	if (chdir_return == -1)
+	{
+		printf("cd: no such file or directory: %s\n", path);
+		return (-1);
+	}
+	return(1);
+	
 }
 
 int	ft_cd(char *path)
 {
 	int		chdir_return;
 	char	*total_path;
-	char	*home_path;
 	
 	total_path = NULL;
-	home_path = NULL;
+	if(!ft_check_path(path))
+		return(-1);
 	if(ft_strncmp(path, "cd", ft_strlen(path)) == 0)
-		home_path = ft_get_home_path(path, 1);
+		return(ft_return_to_home(path));
 	else
 	{
 		path = ft_trim_path(path);
@@ -67,9 +99,7 @@ int	ft_cd(char *path)
 				return (-1);
 		}
 	}
-	if(home_path)
-		chdir_return = chdir(home_path);
-	else if(total_path)
+	if(total_path)
 	{
 		chdir_return = chdir(total_path);
 		free(total_path);
--- srcs/parser/tokenizer/tokenize.c
+++ srcs/parser/tokenizer/tokenize.c
@@ -6,7 +6,7 @@
 /*   By: cw3l <cw3l@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/24 20:36:54 by cw3l              #+#    #+#             */
-/*   Updated: 2025/04/09 22:00:33 by cw3l             ###   ########.fr       */
+/*   Updated: 2025/04/10 08:40:45 by cw3l             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,7 @@ int ft_count_number_of_arg(char **split)
 
     i = 1;
     while (split[i] && ft_get_token(split[i])  == WORD)
-    {
         i++;
-    }
     return(i - 1);
 }
 
@@ -235,6 +233,7 @@ t_token *ft_tokenize(char *str)
                 char *cmd_with_arg = ft_join_cmd_and_arg(&split[i]);
                 //create new node
                 new_node = ft_new_token_node(cmd_with_arg, token);
+
                 // increment by the number of arg.
                 i += args;
             }
