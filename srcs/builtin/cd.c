282fd3091a1421abba86d8d273b19a0359a0a5d7
cb <cdricw3l@gmail.com>
2025-04-10 11:34:40 +0200

    minishell/2025-04-10T11:34:39+02:00

--- srcs/builtin/cd.c
+++ srcs/builtin/cd.c
@@ -6,7 +6,7 @@
 /*   By: cw3l <cw3l@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/26 12:30:49 by cbouhadr          #+#    #+#             */
-/*   Updated: 2025/04/10 10:21:25 by cw3l             ###   ########.fr       */
+/*   Updated: 2025/04/10 11:34:01 by cw3l             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,7 +56,7 @@ int ft_check_path(char *str)
 	if(ft_get_split_len(split) == 3)
 		printf("cd: string not in pwd: %s\n", split[1]);
 	if(ft_get_split_len(split) > 3)
-		printf("cd: too many arguments");
+		printf("cd: too many arguments\n");
 	ft_split_clean(&split);
 	return(0);
 }
@@ -82,30 +82,36 @@ int ft_return_to_home(char *path)
 int	ft_cd(char *path)
 {
 	int		chdir_return;
-	char	*total_path;
 	
-	total_path = NULL;
+	/* 
+		first i check if the path is valide and display the specific message if not.
+		valide arg: cd + 1 argument.
+	*/
 	if(!ft_check_path(path))
 		return(-1);
+	/* 
+		after, i check if cd had argument. If not, we go home.
+	*/
 	if(ft_strncmp(path, "cd", ft_strlen(path)) == 0)
 		return(ft_return_to_home(path));
 	else
 	{
+		/* 
+			We need to separate the cd command from the path and use only the path.
+		*/
 		path = ft_trim_path(path);
 		if (path[0] == '~' )
 		{
-			total_path = ft_get_home_path(path, 0);
-			if (!total_path)
-				return (-1);
+			/* 
+				if the commande had argument, if the argument start by ~ , the home path is the start point.
+			*/
+			path = ft_get_home_path(path, 0);
 		}
 	}
-	if(total_path)
-	{
-		chdir_return = chdir(total_path);
-		free(total_path);
-	}
-	else
-		chdir_return = chdir(path);
+	/* 
+		follow de path. see man chdir, check the return value. Display error message if necessary.
+	*/
+	chdir_return = chdir(path);
 	if (chdir_return == -1)
 	{
 		printf("cd: no such file or directory: %s\n", path);
